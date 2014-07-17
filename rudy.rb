11d8f80613e399486a806179249b6de76eb68da1
Benjamin Walden <bwalden@benjwalden.com>
2014-07-17 01:39:18 -0400

    Create rudy.rb
    Just a little test file I am creating learning Ruby.

---
+++ rudy.rb
@@ -0,0 +1,13 @@
+print "Is this your fisrt time following "first project"
+first_project = gets.chomp
+first_project.capitalize!
+
+print "Whats your name?"
+name = gets.chomp
+name.capitalize!
+
+print "What state are you from?"
+state = gets.chomp
+state.captialize!
+
+puts "#{first_project}, this is your first time! Your name is #{name} and your state is #{state}! Hope you enjoy and follow for more coding updates!"
